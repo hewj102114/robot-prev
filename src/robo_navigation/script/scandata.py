934d1d33de7d1b0b412355b981baa814baeaceb5
Robot No.2 <Robot No.2>
2018-04-27 22:17:04 +0800

    test the threshold of scan data using scandata.py

--- src/robo_navigation/script/scandata.py
+++ src/robo_navigation/script/scandata.py
@@ -5,27 +5,49 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt 
 from sensor_msgs.msg import LaserScan
+from numpy import inf
+loop=5
+thr=0
+#front 0.38  edge 0.42
+# left-right 0.29 edge 0.33
 
 def callback_scan(data):
+    
+    global loop
+    global thr
     rho=data.ranges
-    #print 'rho'
+    rho=np.array(rho)
+    rho[rho<0.25]=inf
+    print 'center dis=%f, edge dis=%f'%(rho[thr+180], rho[thr+150])
     theta=np.arange(0,2*np.pi,np.pi/180)
     #plt.figure()
-    plt.subplot(121,polar=True) 
-    plt.plot(theta,rho,lw=2)
-    plt.plot(theta[19],rho[19],lw=10)
-    plt.draw()
-    plt.pause(0.0000000001)
-    plt.clf()
+    
+    loop=loop-1
+    if loop==0:
+	plt.subplot(121,polar=True) 
+	plt.plot(theta,rho,lw=2)
+	plt.plot(theta[thr],rho[thr],lw=10,color='red')
+	plt.plot(theta[thr+30:thr+32],rho[thr+30:thr+32],lw=10,color='red')
+	plt.plot(theta[thr-30+360:thr-28+360],rho[thr-30+360:thr-28+360],lw=10,color='red')
+	plt.plot(theta[thr+90:thr+92],rho[thr+90:thr+92],lw=10,color='yellow')
+	plt.plot(theta[thr+180:thr+182],rho[thr+180:thr+182],lw=10,color='blue')
+	plt.plot(theta[thr+270:thr+272],rho[thr+270:thr+272],lw=10,color='black')
+	plt.draw()
+	plt.pause(0.000000000001)
+	plt.clf()
+	loop=10
+    
 
 
-if __name__ == '__main__':
-    rospy.init_node('scandata')
 
-    sublidar = rospy.Subscriber('/scan', LaserScan, callback_scan)
-    plt.ion()
-    plt.show()
-    rospy.spin()
+rospy.init_node('scandata')
+
+sublidar = rospy.Subscriber('/scan', LaserScan, callback_scan)
+    
+  
+plt.ion()
+plt.show()
+rospy.spin()
 
 
 
