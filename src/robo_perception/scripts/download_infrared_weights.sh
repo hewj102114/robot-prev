edf8dc51208e5ca2181562b7486b190475973bf2
jwwangchn <jwwangchn@outlook.com>
2018-04-27 10:44:58 +0800

    jwwangchn: add weights files

---
+++ src/robo_perception/scripts/download_infrared_weights.sh
@@ -0,0 +1,7 @@
+mkdir -p ./weights/Infrared-Image
+
+wget -P ./weights/Infrared-Image https://raw.githubusercontent.com/jwwangchn/Public-Downloads/master/ICRA2018/SqueezeDet-Model/Infrared-Image/model.ckpt-99000.data-00000-of-00001
+
+wget -P ./weights/Infrared-Image https://raw.githubusercontent.com/jwwangchn/Public-Downloads/master/ICRA2018/SqueezeDet-Model/Infrared-Image/model.ckpt-99000.index
+
+wget -P ./weights/Infrared-Image https://raw.githubusercontent.com/jwwangchn/Public-Downloads/master/ICRA2018/SqueezeDet-Model/Infrared-Image/model.ckpt-99000.meta
