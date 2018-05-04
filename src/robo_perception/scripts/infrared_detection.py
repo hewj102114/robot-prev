1682ddf7ee74c2542b2d9652d7f53dbb06feb165
eleboss <eleboss>
2018-05-04 18:08:24 +0800

    jwwangchn: finish enemy self identify

--- src/robo_perception/scripts/infrared_detection.py
+++ src/robo_perception/scripts/infrared_detection.py
@@ -87,7 +87,39 @@ def DetectInit():
     sess = tf.Session(config=config)
     saver.restore(sess, checkpoint)
 
+def enemy_self_identify(rgb_image, robo_bboxes, show_image=False):
+    enemy = []
+    for robo_bbox in robo_bboxes:
+        cx, cy, w, h = robo_bbox
+        resize_const = 620.0
+        aligned_w = w * (848.0 / resize_const)
+        aligned_h = h * (848.0 / resize_const)
+        aligned_cx = (cx - 424) * (848.0 / resize_const) + 424.0 + 15.0
+        alinged_cy = (cy - 240) * (848.0 / resize_const) + 240.0
 
+        aligned_robo_bbox = [aligned_cx, alinged_cy, aligned_w, aligned_h]
+        x_min = int(aligned_robo_bbox[0] - aligned_robo_bbox[2]/2)
+        x_max = int(aligned_robo_bbox[0] + aligned_robo_bbox[2]/2)
+        y_min = int(aligned_robo_bbox[1] - aligned_robo_bbox[3]/2)
+        y_max = int(aligned_robo_bbox[1] + aligned_robo_bbox[3]/2)
+        # rgb -> bgr
+        robo_image = rgb_image[y_min:y_max, x_min:x_max, ::-1]
+        h_, w_, c_ = robo_image.shape
+        # print(robo_image)
+        b_channel = np.sum(robo_image[int(h_/2):-1,:,0])
+        g_channel = np.sum(robo_image[int(h_/2):-1,:,1])
+        r_channel = np.sum(robo_image[int(h_/2):-1,:,2])
+        if b_channel < r_channel:
+            judge = 1       # enemy
+        else:
+            judge = 0       # self
+        if show_image:
+            cv2.rectangle(rgb_image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255,0,0), 2)
+            # cv2.rectangle(im, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255,0,0), 2)
+            cv2.imshow('rgb', rgb_image)
+            cv2.imshow('robo_image', robo_image)
+        enemy.append(judge)
+    return enemy
 
 def TsDet_callback(infrared_image, pointcloud):
     print("====================================new image======================================")
@@ -126,7 +158,6 @@ def TsDet_callback(infrared_image, pointcloud):
     final_boxes, final_probs, final_class = model.filter_prediction(det_boxes[0],
                                                                     det_probs[0],
                                                                     det_class[0])
-
     # 按照阈值筛选bbox
     keep_idx = np.squeeze(np.argwhere(
         np.array(final_probs) > mc.PLOT_PROB_THRESH))
@@ -152,8 +183,7 @@ def TsDet_callback(infrared_image, pointcloud):
 
     # 0 -> red, 1 -> wheel, 2 -> blue
     # 检测到车并且检测到轮子, 进入此 if, 目的是为了减少误检率
-    aligned_robo_bbox = []
-    robo_bbox = []
+    robo_bboxes = []
     if len(final_boxes) > 0 and np.any(final_class == 0) and np.any(final_class == 1):
         #judge detect how much robots
         robot_final_idx = np.array(np.where(final_class != 1))
@@ -161,13 +191,7 @@ def TsDet_callback(infrared_image, pointcloud):
             robo_idx = robot_final_idx[0, _]
             robo_bbox = final_boxes[robo_idx, :]
             cx, cy, w, h = robo_bbox[0], robo_bbox[1], robo_bbox[2], robo_bbox[3]   # 获取车bbox的坐标, 宽度和高度
-            const = 620.0
-            aligned_w = w * (848.0 / const)
-            aligned_h = h * (848.0 / const)
-            aligned_cx = (cx - 424) * (848.0 / const) + 424.0 + 15.0
-            alinged_cy = (cy - 240) * (848.0 / const) + 240.0
-            aligned_robo_bbox = [aligned_cx, alinged_cy, aligned_w, aligned_h]
-
+            robo_bboxes.append(robo_bbox)
 
             # 用于提取点云的范围大小
             pointcloud_w = 5
@@ -218,6 +242,7 @@ def TsDet_callback(infrared_image, pointcloud):
             if mc.DEBUG:
                 print('enemy position:', avgX, avgY, avgZ)
 
+        enemy_self_list = enemy_self_identify(align_image, robo_bboxes)
         # 检测完敌人, 对得到的距离信息进行处理
         # tf 包转换
         br = tf2_ros.TransformBroadcaster()
@@ -233,7 +258,8 @@ def TsDet_callback(infrared_image, pointcloud):
         if robo_position.shape[0] == 1:
             robo_position = np.array(robo_position)
 
-        robot_idx = 0
+        red_idx = 0
+        blue_idx = 0
         for object_idx in range(robo_position.shape[0]):
             # ROS 中发送数据
             enemy = Object()
@@ -244,10 +270,16 @@ def TsDet_callback(infrared_image, pointcloud):
 
             t.header.stamp = rospy.Time.now()
             t.header.frame_id = 'realsense_camera'
-
-            t.child_frame_id = 'robot' + str(robot_idx)
-            enemy.team.data = 'robot' + str(robot_idx)
-            robot_idx = robot_idx + 1
+            if enemy_self_list[object_idx] == 1:    # enemy(red)
+                str_enemy_self = 'red'
+                t.child_frame_id = str_enemy_self + str(red_idx)
+                enemy.team.data = str_enemy_self + str(red_idx)
+                red_idx = red_idx + 1
+            else:
+                str_enemy_self = 'blue'
+                t.child_frame_id = str_enemy_self + str(blue_idx)
+                enemy.team.data = str_enemy_self + str(blue_idx)
+                blue_idx = blue_idx + 1
 
             t.transform.translation.x = robo_position[object_idx, 2]
             t.transform.translation.y = -robo_position[object_idx, 0]
@@ -311,18 +343,7 @@ def TsDet_callback(infrared_image, pointcloud):
                 '/home/ubuntu/robot/src/robo_perception/scripts/visual', 'out_' + file_name)
             im = im.astype('uint8')
             cv2.imwrite(out_file_name, im)
-
-        print(aligned_robo_bbox, robo_bbox)
         if mc.SHOW:
-            # cx, cy, w, h
-            if len(aligned_robo_bbox) != 0:
-                x_min = aligned_robo_bbox[0] - aligned_robo_bbox[2]/2
-                x_max = aligned_robo_bbox[0] + aligned_robo_bbox[2]/2
-                y_min = aligned_robo_bbox[1] - aligned_robo_bbox[3]/2
-                y_max = aligned_robo_bbox[1] + aligned_robo_bbox[3]/2
-                cv2.rectangle(align_image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255,0,0), 2)
-            # cv2.rectangle(im, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255,0,0), 2)
-            cv2.imshow('rgb', align_image)
             im = im.astype('uint8')
             cv2.imshow('demo', im)
             cv2.waitKey(3)
